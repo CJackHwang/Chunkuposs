23d1d33a9004e0b83317a925913aa12632bca782
CJACK <tetr20071102@gmail.com>
2025-01-21 23:02:41 +0800

    更新4.0.9

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
-import { createApp } from 'vue'
-
-import App from './App.vue'
-import './assets/styles.css'
-import 'toastify-js/src/toastify.css'
-
-createApp(App).mount('body')
+import { createApp } from 'vue';
+import App from './App.vue';
+import './assets/styles.css';
+import 'toastify-js/src/toastify.css';
+createApp(App).mount('body');
