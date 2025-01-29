8b7f3c173e9ed49e08a4408cd0747633c2fa3e26
CJACK <tetr20071102@gmail.com>
2025-01-30 04:05:10 +0800

    添加PWA支持，更新版本号至5.1.5，注册服务工作者并配置缓存策略

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,64 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
+import { VitePWA } from 'vite-plugin-pwa';
+const mode = 'production';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
+    VitePWA({
+      mode: 'development',
+      base: '/',
+      manifest: {
+        name: 'FlowChunkFlex',
+        short_name: 'FCF微云盘',
+        theme_color: '#1A1C23',
+        description: '流式分块上传工具，使用编程猫七牛云对象存储接口开发的微云盘',
+        icons: [
+          {
+            src: '/app.png',
+            sizes: '1024x1024',
+            type: 'image/png',
+          },
+        ],
+      },
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,jpg,svg,woff2,ttf}'],
+        runtimeCaching: [
+          // 配置自定义运行时缓存
+          {
+            urlPattern: ({ url }) =>
+              url.origin === 'https://ik.imagekit.io',
+            handler: 'StaleWhileRevalidate',
+            options: {
+              cacheName: 'cdn-styles-fonts',
+              expiration: {
+                maxEntries: 30, // 最多缓存30个文件
+                maxAgeSeconds: 30 * 24 * 60 * 60 // 30天
+              },
+              cacheableResponse: { statuses: [200] }
+            }
+          },
+          {
+            urlPattern: /\.(?:js|css|html|json|ico|png|jpg|jpeg|svg|woff2|ttf)$/,
+            handler: 'StaleWhileRevalidate',
+            options: {
+              cacheName: 'static-assets',
+              expiration: { maxEntries: 100, maxAgeSeconds: 30 * 24 * 60 * 60 },
+              cacheableResponse: { statuses: [200] }
+            }
+          }
+        ]
+      },
+      devOptions: {
+        enabled: true,
+        type: 'module',
+        navigateFallback: 'index.html'
+      },
+    }),
   ],
   resolve: {
     alias: {
